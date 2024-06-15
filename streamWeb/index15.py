0403d09a2d7b0ad0edeb4dcbffb6a43c121d9c9a
林惠萍 <168253529+lynnhplin@users.noreply.github.com>
2024-06-15 08:11:51 +0000

    modify_4

--- streamWeb/index15.py
+++ streamWeb/index15.py
@@ -4,9 +4,10 @@ st.title("計數器範例")
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
-#利用session_state的寫法
-increment = st.button("Increment",key = 'Increment')
-st.session_state
-if increment:
+#利用call_back的寫法
+def increment_counter():
     st.session_state.count += 1
+
+st.button("Increment",key="Increment",on_click=increment_counter)
+
 st.write("Count=",st.session_state.count)    
