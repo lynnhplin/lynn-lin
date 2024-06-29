f30b6e82692fa5811339e1889bcff9d443e7b4f1
林惠萍 <168253529+lynnhplin@users.noreply.github.com>
2024-06-29 04:03:49 +0000

    modify-l13-4

--- l13/index2.py
+++ l13/index2.py
@@ -10,10 +10,17 @@ try:
 except Exception as e:
     st.error(e)
 else:
-
     root = Root.model_validate_json(data_str)
     data = root.model_dump()
     areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
 
+    def area_change():
+        st.write(st.session_state.sarea)
+
+    if 'sarea' not in st.session_state:
+        st.session_state.sarea = "淡水區"
+
     with st.sidebar:
-        st.selectbox(":orange[請選擇行政區域:]",options=areas)
+        st.selectbox(":orange[請選擇行政區域:]",options=areas,on_change=area_change,key='sarea')
+    
+    st.write(st.session_state)
