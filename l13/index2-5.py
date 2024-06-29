8b3f9fc48fb4013edae41befab9325ad5a46e6c0
林惠萍 <168253529+lynnhplin@users.noreply.github.com>
2024-06-29 08:19:39 +0000

    modify_l13-6

--- l13/index2-5.py
+++ l13/index2-5.py
@@ -5,7 +5,6 @@ import source
 from source import Root
 import pandas as pd
 
-
 try:
     data_str = source.download_youbike()
 except Exception as e:
@@ -15,7 +14,8 @@ else:
     data = root.model_dump()
     areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
 
-    
+    st.title("新北市youbike各行政區站點資料")
+    tableContainer = st.container(border=False)
     
     def area_change():
         sarea_name = st.session_state.sarea
@@ -24,51 +24,37 @@ else:
         for item in data:
             if item['行政區'] == sarea_name:
                 display_data.append(item)
-
-        st.title("新北市youbike各行政區站點資料")
-            
-        
-        col1, col2 = st.columns([1, 6])
-        with col2:
-            df1 = pd.DataFrame(display_data,
-                            columns=['站點名稱','總數','可借','可還'])
-            st.dataframe(data=df1)
-
-        with col1:
+        with tableContainer:
             st.subheader(sarea_name)
 
-        tableContainer = st.container(border=False)
-        with tableContainer:  
-                        
-            
+            df1= pd.DataFrame(display_data,columns=['站點名稱','日期時間','地址','總數','可借','可還'])
+            st.dataframe(data=df1)
+         
             df0 = pd.DataFrame(display_data,
                                columns=['站點名稱','總數'])
-            
             st.scatter_chart(df0,
                              x='站點名稱',
                              y='總數',
                              color='#00ff00',
-                             size='總數')
-            
+                             size='總數') 
+          
+            df2 =pd.DataFrame(display_data,
+                              columns=['站點名稱','總數','可借'])
 
-            df2 = pd.DataFrame(display_data,
-                               columns=['站點名稱','總數','可借'])
-            
             st.scatter_chart(df2,
                              x='站點名稱',
                              y='可借',
                              size='可借')
             
-            df3 = pd.DataFrame(display_data,
-                               columns=['站點名稱','總數','可還'])
-            
+            df3 =pd.DataFrame(display_data,
+                              columns=['站點名稱','總數','可還'])
+
             st.scatter_chart(df3,
                              x='站點名稱',
                              y='可還',
                              color='#ff0000',
                              size='可還')
 
-
-
     with st.sidebar:
         st.selectbox(":orange[請選擇行政區域:]",options=areas,on_change=area_change,key='sarea')
+    
